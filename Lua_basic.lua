7a540aaab1460cf7c6678bec88fb4cfe65bb6a44
Zorro30 <gbp301195@gmail.com>
2018-12-06 23:09:18 +0530

    My first Lua code

--- Lua_basic.lua
+++ Lua_basic.lua
@@ -20,3 +20,22 @@ a = 10
 b = 30
 a, b = b, a
 io.write('Value after swapping : ', a ,' ', b ,'\n')
+
+--> Data Types
+
+print(type("What is my type"))   --> string
+t = 10
+
+print(type(5.8*t))               --> number
+print(type(true))                --> boolean
+print(type(print))               --> function
+print(type(nil))                 --> nil
+print(type(type(ABC)))           --> string
+
+--> concatenates two string
+
+a = "Hello"
+b = "World"
+a = a .. b
+print(a)
+
